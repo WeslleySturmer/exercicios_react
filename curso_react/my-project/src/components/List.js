dc05f50d34ecc9623f37333d47c9bc8649102db5
WeslleySturmann <weslley.w.sturmer@gmail.com>
2025-09-30 15:57:37 -0300

    Aula 8 - Avançando em props - propTypes e defaultProp

--- curso_react/my-project/src/components/List.js
+++ curso_react/my-project/src/components/List.js
@@ -5,9 +5,9 @@ function List(){
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Lamborghini"/>
-                <Item marca="Audi"/>
-                <Item marca="BWM"/>
+                <Item marca="Lamborghini" lancamento={2025}/>
+                <Item marca="Audi" lancamento={2022}/>
+                <Item marca="BWM" lancamento={2018}/>
             </ul>
         </>
     )
